bcaff77093b95380ca5437274a80802d2775efa9
Bram <70259613+Bram1903@users.noreply.github.com>
2024-02-06 09:59:15 +0100

    Update code comments and refactor UpdateNotifier class
    The commit enhances the code readability in the UpdateNotifier class by adding comments and updating variable names. It also streamlines the update notification message creation process. Additionally, unnecessary white spaces have been removed to have a cleaner code structure.

--- src/main/java/com/deathmotion/antihealthindicator/events/UpdateNotifier.java
+++ src/main/java/com/deathmotion/antihealthindicator/events/UpdateNotifier.java
@@ -14,37 +14,50 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+/**
+ * This class is responsible for notifying when an update is available.
+ */
 public class UpdateNotifier implements Listener {
 
     private final AntiHealthIndicator plugin;
-
     private final BukkitAudiences adventure;
+    private final Component updateComponent;
 
-    private final Component component;
-
+    /**
+     * Constructor for the UpdateNotifier
+     *
+     * @param plugin        instance of AntiHealthIndicator plugin
+     * @param latestVersion the latest version of the plugin
+     */
     public UpdateNotifier(AntiHealthIndicator plugin, String latestVersion) {
         this.plugin = plugin;
         this.adventure = plugin.getAdventure();
 
-        String downloadLink = "https://www.spigotmc.org/resources/antihealthindicator.114851//";
-        this.component = Component.text()
-                .append(Component.text("[AntiHealthIndicator] ", NamedTextColor.RED))
+        // preparing the update notification message
+        this.updateComponent = Component.text()
+                .append(Component.text("[AntiHealthIndicator] ", NamedTextColor.RED)
+                        .decoration(TextDecoration.BOLD, true))
                 .append(Component.text("Version " + latestVersion + " is ", NamedTextColor.GREEN))
                 .append(Component.text("now available", NamedTextColor.GREEN)
                         .decorate(TextDecoration.UNDERLINED)
                         .hoverEvent(HoverEvent.showText(Component.text("Click to download", NamedTextColor.GREEN)))
-                        .clickEvent(ClickEvent.openUrl(downloadLink)))
-                .append(Component.text("! ", NamedTextColor.GREEN))
+                        .clickEvent(ClickEvent.openUrl("https://www.spigotmc.org/resources/antihealthindicator.20803/")))
+                .append(Component.text("!", NamedTextColor.GREEN))
                 .build();
     }
 
+    /**
+     * Event listener for player joining the game
+     *
+     * @param event encapsulates information about the player join event
+     */
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
         if (player.hasPermission("AntiHealthIndicator.Notify")) {
             FoliaCompatUtil.runTaskTimerAsync(this.plugin, object -> {
-                adventure.player(player).sendMessage(component);
+                adventure.player(player).sendMessage(updateComponent);
             }, 40L, 20L * 60 * 60);
         }
     }
