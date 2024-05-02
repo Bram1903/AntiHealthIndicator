b0de9f93f978537e8ea19656ca7f73a479dbda2b
Bram <bram@bramdekker.com>
2024-05-02 16:01:06 +0200

    Updated: Run Server to use 1.20.6, in preparation for the 1.20.5+ release.

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
 tasks {
     runServer {
         // The version of the server to run
-        val version = "1.20.4"
+        val version = "1.20.6"
 
         minecraftVersion(version)
         runDirectory.set(file("server/$version"))
@@ -15,15 +15,17 @@ tasks {
         // 1.8.8 - 1.16.5 = Java 8
         // 1.17           = Java 16
         // 1.18 - 1.20.4  = Java 17
+        // 1-20.5+        = Java 21
         javaLauncher.set(project.javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(JavaLanguageVersion.of(21))
         })
 
         downloadPlugins {
             url("https://ci.codemc.io/job/retrooper/job/packetevents/lastBuild/artifact/spigot/build/libs/packetevents-spigot-2.2.1.jar")
-            url("https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar")
             url("https://ci.lucko.me/job/spark/400/artifact/spark-bukkit/build/libs/spark-1.10.59-bukkit.jar")
             url("https://download.luckperms.net/1530/bukkit/loader/LuckPerms-Bukkit-5.4.117.jar")
+            url("https://github.com/ViaVersion/ViaVersion/releases/download/4.10.0/ViaVersion-4.10.0.jar")
+            url("https://github.com/ViaVersion/ViaBackwards/releases/download/4.10.0/ViaBackwards-4.10.0.jar")
         }
 
         jvmArgs = listOf(
