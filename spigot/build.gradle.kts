b89e3cddabd383c2a27ef51658e903df6d3dcca6
Bram <bram@bramdekker.com>
2024-05-06 13:13:26 +0200

    Improved: runServer tasks

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -9,33 +9,48 @@ dependencies {
 }
 
 tasks {
-    runPaper.folia.registerTask()
+    // 1.8.8 - 1.16.5 = Java 8
+    // 1.17           = Java 16
+    // 1.18 - 1.20.4  = Java 17
+    // 1-20.5+        = Java 21
+    val version = "1.20.6"
+    val javaVersion = 21
+
+    val requiredPlugins = runPaper.downloadPluginsSpec {
+        url("https://ci.codemc.io/job/retrooper/job/packetevents/lastBuild/artifact/spigot/build/libs/packetevents-spigot-2.2.1.jar")
+    }
+
+    val jvmArgsExternal = listOf(
+        "-Dcom.mojang.eula.agree=true"
+    )
 
     runServer {
-        // The version of the server to run
-        val version = "1.20.6"
-
         minecraftVersion(version)
-        runDirectory.set(file("server/$version"))
+        runDirectory.set(file("server/paper/$version"))
 
-        // 1.8.8 - 1.16.5 = Java 8
-        // 1.17           = Java 16
-        // 1.18 - 1.20.4  = Java 17
-        // 1-20.5+        = Java 21
         javaLauncher.set(project.javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(21))
+            languageVersion.set(JavaLanguageVersion.of(javaVersion))
         })
 
+        downloadPlugins.from(requiredPlugins)
         downloadPlugins {
-            url("https://ci.codemc.io/job/retrooper/job/packetevents/lastBuild/artifact/spigot/build/libs/packetevents-spigot-2.2.1.jar")
             url("https://ci.lucko.me/job/spark/410/artifact/spark-bukkit/build/libs/spark-1.10.65-bukkit.jar")
             url("https://download.luckperms.net/1530/bukkit/loader/LuckPerms-Bukkit-5.4.117.jar")
-            url("https://github.com/ViaVersion/ViaVersion/releases/download/4.10.1/ViaVersion-4.10.1.jar")
-            url("https://github.com/ViaVersion/ViaBackwards/releases/download/4.10.1/ViaBackwards-4.10.1.jar")
         }
 
-        jvmArgs = listOf(
-            "-Dcom.mojang.eula.agree=true"
-        )
+        jvmArgs = jvmArgsExternal
+    }
+
+    runPaper.folia.registerTask() {
+        minecraftVersion(version)
+        runDirectory.set(file("server/folia/$version"))
+
+        javaLauncher.set(project.javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(javaVersion))
+        })
+
+        downloadPlugins.from(requiredPlugins)
+
+        jvmArgs = jvmArgsExternal
     }
 }
