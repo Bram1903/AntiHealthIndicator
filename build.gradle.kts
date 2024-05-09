1dfd6953cc89a160ac58dc30112b2dd5add25c5e
Bram <bram@bramdekker.com>
2024-05-09 12:25:47 +0200

    Fixed: Relocation issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ tasks {
 
         relocate("com.github.benmanes.caffeine", "com.deathmotion.antihealthindicator.shaded.caffeine")
         relocate("net.kyori.adventure.text.serializer.gson", "io.github.retrooper.packetevents.adventure.serializer.gson")
+        relocate("net.kyori.adventure.text.serializer.legacy", "io.github.retrooper.packetevents.adventure.serializer.legacy")
         minimize()
     }
 }
