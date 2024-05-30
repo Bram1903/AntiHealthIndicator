1cf545c360819cc84bbb3fad5746b68c789b36ab
Bram <bram@bramdekker.com>
2024-05-30 10:06:41 +0200

    Added: Relocation for some libraries

--- buildSrc/src/main/kotlin/antihealthindicator.shadow-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/antihealthindicator.shadow-conventions.gradle.kts
@@ -7,6 +7,16 @@ tasks {
     shadowJar {
         archiveFileName = "AntiHealthIndicator-${project.version}.jar"
         archiveClassifier = null
+
+        relocate("com.github.benmanes.caffeine", "com.deathmotion.antihealthindicator.shaded.caffeine")
+        relocate(
+            "net.kyori.adventure.text.serializer.gson",
+            "io.github.retrooper.packetevents.adventure.serializer.gson"
+        )
+        relocate(
+            "net.kyori.adventure.text.serializer.legacy",
+            "io.github.retrooper.packetevents.adventure.serializer.legacy"
+        )
     }
 
     assemble {
