3efecfcaefa26fd315af4081d7560de9c23ae972
Bram <bram@bramdekker.com>
2024-05-30 11:18:31 +0200

    Added: Gradle Enterprise Builds Scan Publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,29 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version ("3.16.2")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             from(files("libs.versions.toml"))
         }
-    }
-}
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
