f14304a6230d19d35a6041ac7113663a467b622a
Bram <bram@bramdekker.com>
2024-06-06 00:02:06 +0200

    Rewrote the ConfigManager

--- common/src/main/java/com/deathmotion/antihealthindicator/managers/ConfigManager.java
+++ common/src/main/java/com/deathmotion/antihealthindicator/managers/ConfigManager.java
@@ -23,12 +23,11 @@ import com.deathmotion.antihealthindicator.data.Settings;
 import com.deathmotion.antihealthindicator.enums.ConfigOption;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Map;
+
+import java.io.File;
 
 public class ConfigManager<P> {
     private final AHIPlatform<P> platform;
@@ -36,78 +35,141 @@ public class ConfigManager<P> {
 
     public ConfigManager(AHIPlatform<P> platform) {
         this.platform = platform;
-
         saveDefaultConfiguration();
         loadConfig();
     }
 
     private void saveDefaultConfiguration() {
-        try {
-            File configFile = new File(platform.getPluginDirectory(), "config.yml");
-            if (!configFile.exists()) {
-                Files.copy(getClass().getResourceAsStream("/config.yml"), configFile.toPath());
+        File configFile = new File(platform.getPluginDirectory(), "config.yml");
+        if (!configFile.exists()) {
+            try (InputStream inputStream = getClass().getResourceAsStream("/config.yml")) {
+                if (inputStream != null) {
+                    Files.copy(inputStream, configFile.toPath());
+                } else {
+                    platform.getLogManager().severe("Default configuration file not found in resources!");
+                }
+            } catch (IOException e) {
+                platform.getLogManager().severe("Failed to save default configuration file: " + e.getMessage());
             }
-        } catch (Exception e) {
-            platform.getLogManager().severe("Failed to save default configuration file!");
         }
     }
 
     private void loadConfig() {
-        try {
-            File configFile = new File(platform.getPluginDirectory(), "config.yml");
+        File configFile = new File(platform.getPluginDirectory(), "config.yml");
+        if (!configFile.exists()) {
+            platform.getLogManager().severe("Config file not found!");
+            platform.commonOnDisable();
+            return;
+        }
 
+        try (InputStream inputStream = Files.newInputStream(configFile.toPath())) {
             Yaml yaml = new Yaml();
-            Map<String, Object> yamlData = yaml.load(new FileInputStream(configFile));
+            Map<String, Object> yamlData = yaml.load(inputStream);
 
             Settings settings = new Settings();
-            settings.setDebug(findNestedValue(yamlData, ConfigOption.DEBUG_ENABLED.getKey().split("\\."), ConfigOption.DEBUG_ENABLED));
-            settings.getUpdateChecker().setEnabled(findNestedValue(yamlData, ConfigOption.UPDATE_CHECKER_ENABLED.getKey().split("\\."), ConfigOption.UPDATE_CHECKER_ENABLED));
-            settings.getUpdateChecker().setPrintToConsole(findNestedValue(yamlData, ConfigOption.UPDATE_CHECKER_PRINT_TO_CONSOLE.getKey().split("\\."), ConfigOption.UPDATE_CHECKER_PRINT_TO_CONSOLE));
-            settings.getUpdateChecker().setNotifyInGame(findNestedValue(yamlData, ConfigOption.NOTIFY_IN_GAME.getKey().split("\\."), ConfigOption.NOTIFY_IN_GAME));
-            settings.setAllowBypass(findNestedValue(yamlData, ConfigOption.ALLOW_BYPASS_ENABLED.getKey().split("\\."), ConfigOption.ALLOW_BYPASS_ENABLED));
-            settings.setWorldSeed(findNestedValue(yamlData, ConfigOption.SPOOF_WORLD_SEED_ENABLED.getKey().split("\\."), ConfigOption.SPOOF_WORLD_SEED_ENABLED));
-            settings.setFoodSaturation(findNestedValue(yamlData, ConfigOption.SPOOF_FOOD_SATURATION_ENABLED.getKey().split("\\."), ConfigOption.SPOOF_FOOD_SATURATION_ENABLED));
-            settings.getEntityData().setEnabled(findNestedValue(yamlData, ConfigOption.ENTITY_DATA_ENABLED.getKey().split("\\."), ConfigOption.ENTITY_DATA_ENABLED));
-            settings.getEntityData().setPlayersOnly(findNestedValue(yamlData, ConfigOption.PLAYER_ONLY.getKey().split("\\."), ConfigOption.PLAYER_ONLY));
-            settings.getEntityData().setAirTicks(findNestedValue(yamlData, ConfigOption.AIR_TICKS_ENABLED.getKey().split("\\."), ConfigOption.AIR_TICKS_ENABLED));
-            settings.getEntityData().setHealth(findNestedValue(yamlData, ConfigOption.HEALTH_ENABLED.getKey().split("\\."), ConfigOption.HEALTH_ENABLED));
-            settings.getEntityData().setIgnoreVehicles(findNestedValue(yamlData, ConfigOption.IGNORE_VEHICLES_ENABLED.getKey().split("\\."), ConfigOption.IGNORE_VEHICLES_ENABLED));
-            settings.getEntityData().getWolves().setEnabled(findNestedValue(yamlData, ConfigOption.IGNORE_WOLVES_ENABLED.getKey().split("\\."), ConfigOption.IGNORE_WOLVES_ENABLED));
-            settings.getEntityData().getWolves().setTamed(findNestedValue(yamlData, ConfigOption.FOR_TAMED_WOLVES_ENABLED.getKey().split("\\."), ConfigOption.FOR_TAMED_WOLVES_ENABLED));
-            settings.getEntityData().getWolves().setOwner(findNestedValue(yamlData, ConfigOption.FOR_OWNED_WOLVES_ENABLED.getKey().split("\\."), ConfigOption.FOR_OWNED_WOLVES_ENABLED));
-            settings.getEntityData().getIronGolems().setEnabled(findNestedValue(yamlData, ConfigOption.IGNORE_IRON_GOLEMS_ENABLED.getKey().split("\\."), ConfigOption.IGNORE_IRON_GOLEMS_ENABLED));
-            settings.getEntityData().getIronGolems().setGradual(findNestedValue(yamlData, ConfigOption.GRADUAL_IRON_GOLEM_HEALTH_ENABLED.getKey().split("\\."), ConfigOption.GRADUAL_IRON_GOLEM_HEALTH_ENABLED));
-            settings.setAbsorption(findNestedValue(yamlData, ConfigOption.ABSORPTION_ENABLED.getKey().split("\\."), ConfigOption.ABSORPTION_ENABLED));
-            settings.setXp(findNestedValue(yamlData, ConfigOption.XP_ENABLED.getKey().split("\\."), ConfigOption.XP_ENABLED));
-            settings.getItems().setEnabled(findNestedValue(yamlData, ConfigOption.ITEMS_ENABLED.getKey().split("\\."), ConfigOption.ITEMS_ENABLED));
-            settings.getItems().setStackAmount(findNestedValue(yamlData, ConfigOption.STACK_AMOUNT_ENABLED.getKey().split("\\."), ConfigOption.STACK_AMOUNT_ENABLED));
-            settings.getItems().setDurability(findNestedValue(yamlData, ConfigOption.DURABILITY_ENABLED.getKey().split("\\."), ConfigOption.DURABILITY_ENABLED));
-            settings.getItems().setEnchantments(findNestedValue(yamlData, ConfigOption.ENCHANTMENTS_ENABLED.getKey().split("\\."), ConfigOption.ENCHANTMENTS_ENABLED));
+            for (ConfigOption option : ConfigOption.values()) {
+                setConfigOption(yamlData, settings, option);
+            }
+
+            this.platform.getLogManager().info("Debug mode is " + (settings.isDebug() ? "enabled" : "disabled"));
 
             this.settings = settings;
-        } catch (FileNotFoundException e) {
-            // Handle the exception...
-            System.out.println("Config file not found!");
-            this.platform.commonOnDisable();
+        } catch (IOException e) {
+            platform.getLogManager().severe("Failed to load configuration: " + e.getMessage());
+            platform.commonOnDisable();
         }
     }
 
-    private boolean findNestedValue(Map<String, Object> yamlData, String[] keys, ConfigOption configOption) {
-        if (keys.length == 0 || yamlData == null) {
-            this.platform.getLogManager().severe("Config value for " + String.join(".", keys) + " not found! Using default value.");
-            return configOption.getDefaultValue();
-        }
+    private void setConfigOption(Map<String, Object> yamlData, Settings settings, ConfigOption option) {
+        String[] keys = option.getKey().split("\\.");
+        Object value = findNestedValue(yamlData, keys, option.getDefaultValue());
 
-        Object value = yamlData.get(keys[0]);
-        if (keys.length > 1) {
-            value = findNestedValue((Map<String, Object>) value, Arrays.copyOfRange(keys, 1, keys.length), configOption);
+        switch (option) {
+            case DEBUG_ENABLED:
+                settings.setDebug((Boolean) value);
+                break;
+            case UPDATE_CHECKER_ENABLED:
+                settings.getUpdateChecker().setEnabled((Boolean) value);
+                break;
+            case UPDATE_CHECKER_PRINT_TO_CONSOLE:
+                settings.getUpdateChecker().setPrintToConsole((Boolean) value);
+                break;
+            case NOTIFY_IN_GAME:
+                settings.getUpdateChecker().setNotifyInGame((Boolean) value);
+                break;
+            case ALLOW_BYPASS_ENABLED:
+                settings.setAllowBypass((Boolean) value);
+                break;
+            case SPOOF_WORLD_SEED_ENABLED:
+                settings.setWorldSeed((Boolean) value);
+                break;
+            case SPOOF_FOOD_SATURATION_ENABLED:
+                settings.setFoodSaturation((Boolean) value);
+                break;
+            case ENTITY_DATA_ENABLED:
+                settings.getEntityData().setEnabled((Boolean) value);
+                break;
+            case PLAYER_ONLY:
+                settings.getEntityData().setPlayersOnly((Boolean) value);
+                break;
+            case AIR_TICKS_ENABLED:
+                settings.getEntityData().setAirTicks((Boolean) value);
+                break;
+            case HEALTH_ENABLED:
+                settings.getEntityData().setHealth((Boolean) value);
+                break;
+            case IGNORE_VEHICLES_ENABLED:
+                settings.getEntityData().setIgnoreVehicles((Boolean) value);
+                break;
+            case IGNORE_WOLVES_ENABLED:
+                settings.getEntityData().getWolves().setEnabled((Boolean) value);
+                break;
+            case FOR_TAMED_WOLVES_ENABLED:
+                settings.getEntityData().getWolves().setTamed((Boolean) value);
+                break;
+            case FOR_OWNED_WOLVES_ENABLED:
+                settings.getEntityData().getWolves().setOwner((Boolean) value);
+                break;
+            case IGNORE_IRON_GOLEMS_ENABLED:
+                settings.getEntityData().getIronGolems().setEnabled((Boolean) value);
+                break;
+            case GRADUAL_IRON_GOLEM_HEALTH_ENABLED:
+                settings.getEntityData().getIronGolems().setGradual((Boolean) value);
+                break;
+            case ABSORPTION_ENABLED:
+                settings.setAbsorption((Boolean) value);
+                break;
+            case XP_ENABLED:
+                settings.setXp((Boolean) value);
+                break;
+            case ITEMS_ENABLED:
+                settings.getItems().setEnabled((Boolean) value);
+                break;
+            case STACK_AMOUNT_ENABLED:
+                settings.getItems().setStackAmount((Boolean) value);
+                break;
+            case DURABILITY_ENABLED:
+                settings.getItems().setDurability((Boolean) value);
+                break;
+            case ENCHANTMENTS_ENABLED:
+                settings.getItems().setEnchantments((Boolean) value);
+                break;
+            default:
+                platform.getLogManager().severe("Unknown config option: " + option);
         }
+    }
 
-        if (value == null) {
-            platform.getLogManager().severe("Config value for " + String.join(".", keys) + " not found! Using default value.");
-            value = configOption.getDefaultValue();
+    private Object findNestedValue(Map<String, Object> yamlData, String[] keys, Object defaultValue) {
+        Object value = yamlData;
+        for (String key : keys) {
+            if (value instanceof Map) {
+                value = ((Map<?, ?>) value).get(key);
+            } else {
+                platform.getLogManager().severe("Invalid config structure for key: " + String.join(".", keys));
+                return defaultValue;
+            }
         }
-
-        return (boolean) value;
+        return value != null ? value : defaultValue;
     }
 }
+
