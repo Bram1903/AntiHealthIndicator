6e91f28b369877cb35f250a1526cbb973bc89fa3
Bram <bram@bramdekker.com>
2024-06-07 19:02:44 +0200

    Version Bump: 2.2.2-SNAPSHOT -> 2.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 group = "com.deathmotion.antihealthindicator"
 description = "A plugin that prevents hackers and modders from seeing the health of other players."
-version = "2.2.2-SNAPSHOT"
+version = "2.2.2"
 
 dependencies {
     implementation(project(":common"))
