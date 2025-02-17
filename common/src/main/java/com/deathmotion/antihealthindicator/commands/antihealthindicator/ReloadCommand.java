e2ffa506c2026f30d972212ea5d752e037d13545
Bram <bram@bramdekker.com>
2025-02-17 11:22:29 +0100

    Updated: Copyright Year

--- common/src/main/java/com/deathmotion/antihealthindicator/commands/antihealthindicator/ReloadCommand.java
+++ common/src/main/java/com/deathmotion/antihealthindicator/commands/antihealthindicator/ReloadCommand.java
@@ -1,9 +1,9 @@
 /*
  * This file is part of AntiHealthIndicator - https://github.com/Bram1903/AntiHealthIndicator
- * Copyright (C) 2024 Bram and contributors
+ * Copyright (C) 2025 Bram and contributors
  *
  * This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
