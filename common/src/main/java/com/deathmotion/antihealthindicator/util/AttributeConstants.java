59782db15d3ff3484874e26c8fd72104aa5144a7
Bram <bram@bramdekker.com>
2025-12-06 13:39:10 +0100

    Removed: Unused AttributeConstants class

--- common/src/main/java/com/deathmotion/antihealthindicator/util/AttributeConstants.java
+++
@@ -1,6 +0,0 @@
-package com.deathmotion.antihealthindicator.util;
-
-public class AttributeConstants {
-    public final static String MAX_HEALTH_KEY = "max_health";
-    public final static String MAX_ABSORPTION_KEY = "max_absorption";
-}
