2ea8487724b9cc16b9bda43037863daed0d18212
Bram <bram@bramdekker.com>
2025-12-07 13:13:47 +0100

    Removed: gradleEnterprise Scan Updated: foojay-resolver-convention from 0.8.0 -> 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,11 @@
 pluginManagement {
     repositories {
-        mavenCentral()
         gradlePluginPortal()
     }
 }
 
 plugins {
-    id("com.gradle.enterprise") version ("3.16.2")
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "AntiHealthIndicator"
