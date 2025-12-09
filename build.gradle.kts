01e3dc9ed4184ead1861a01ae795b23d1557d850
Bram <bram@bramdekker.com>
2025-12-09 23:23:12 +0100

    Bumped version from 2.5.0 -> 2.5.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 group = "com.deathmotion.antihealthindicator"
 description = "A plugin that prevents hackers and modders from seeing the health of other players."
-val fullVersion = "2.5.0"
-val snapshot = false
+val fullVersion = "2.5.1"
+val snapshot = true
 
 fun getVersionMeta(includeHash: Boolean): String {
     if (!snapshot) {
